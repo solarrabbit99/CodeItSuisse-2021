88421589d23d9f470f8c3fe8ad52ef75898f09cc
Lee Zheng Han <e0544595@u.nus.edu>
2021-09-25 20:49:55 +0800

    Improve guess

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -123,7 +123,7 @@ public class Main {
   @ResponseBody
   public String fixedRace(@RequestBody String fixedRace) {
     System.out.println(fixedRace);
-    return "Margit Mello, Jesse Julio, Duane Darrel, foo, foo, foo, foo, foo, foo, foo";
+    return "Franklin Filippi, Margit Mello, Jesse Julio, Duane Darrel, foo, foo, foo, foo, foo, foo";
   }
 
   @Bean
